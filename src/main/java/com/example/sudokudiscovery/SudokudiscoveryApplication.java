4e15ae0287bd41085baf3026bf3ebcf737006ac0
Lore <marvinpitz@gmx.de>
2023-05-21 15:47:17 +0200

    initial spring commit

---
+++ src/main/java/com/example/sudokudiscovery/SudokudiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sudokudiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokudiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudokudiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sudokudiscovery/SudokudiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sudokudiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokudiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
